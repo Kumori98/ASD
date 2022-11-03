e3a3359d892e7a925c96bb64277d4d41fcf5f0b7
Giovanni Pizzenti <giannipizzenti98@gmail.com>
2022-11-03 15:18:38 +0100

    Main

--- C/Lab4/4.1/4.1.c
+++ C/Lab4/4.1/4.1.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #define NOMEFILE "grafo.txt"
@@ -16,6 +17,9 @@ typedef struct grafo{
 void carica_grafo(FILE *fp, grafo_t *grafo);
 void vertex_cover(int pos, grafo_t *grafo, int *sol);
 void is_valid(grafo_t *grafo, int *sol);
+void node_without_edge(grafo_t *grafo);
+arco_t crea_cappio(int nodo);
+void inserisci_arco(grafo_t *grafo, arco_t arco);
 
 int main(){
     grafo_t *grafo;
@@ -30,6 +34,8 @@ int main(){
 
     carica_grafo(fp, grafo);
     fclose(fp);
+
+    node_without_edge(grafo); //caso particolare grafo non completamente connesso
     
     sol = (int *) malloc(grafo->N*sizeof(int));
     vertex_cover(0, grafo, sol);
@@ -48,6 +54,7 @@ void carica_grafo(FILE *fp, grafo_t *grafo){
         fscanf(fp, "%d %d", &grafo->archi[i].nodo1, &grafo->archi[i].nodo2);
     }
 }
+
 
 void vertex_cover(int pos, grafo_t *grafo, int *sol){
     int i;
@@ -88,5 +95,36 @@ void is_valid(grafo_t *grafo, int *sol){
     printf("} \n");
 }
 
+void node_without_edge(grafo_t *grafo){
+    int *node_occ, i, j;
 
+    node_occ = (int *) calloc(grafo->N,sizeof(int));
 
+    for(i=0; i<grafo->N; i++){
+        for(j=0; j<grafo->E; j++){
+            if((grafo->archi[j].nodo1 == i) || (grafo->archi[j].nodo2 == i)){
+                node_occ[i]=1;
+                break;
+            }
+        }    
+    }
+    for(i=0; i<grafo->N; i++){
+        if(node_occ[i]==0){
+            inserisci_arco(grafo, crea_cappio(i));
+        }
+    }
+}
+
+arco_t crea_cappio(int nodo){
+    arco_t cappio;
+    cappio.nodo1 = nodo;
+    cappio.nodo2 = -1;
+    return cappio;
+}
+
+void inserisci_arco(grafo_t *grafo, arco_t arco){
+    grafo->E++;
+    grafo->archi = (arco_t *)realloc(grafo->archi,(grafo->E)*sizeof(arco_t));
+    grafo->archi[(grafo->E)-1].nodo1 = arco.nodo1;
+    grafo->archi[(grafo->E)-1].nodo2 = arco.nodo2;
+}
