4629114e380b539e4b8c4bc5046f1d54c7277569
Giovanni Pizzenti <giannipizzenti98@gmail.com>
2022-11-06 13:54:18 +0100

    Main

--- C/Lab5/5.2/5.2.c
+++ C/Lab5/5.2/5.2.c
@@ -23,8 +23,9 @@ typedef struct board{
 void carica_tiles(FILE *fp, options_t *options);
 void carica_board(FILE *fp, board_t *board, options_t *options);
 void mostra_best(board_t *board, options_t *options);
-int calcola_best(int pos, options_t *val, int *sol, int *b_sol, int *mark, int n, int nr, int nc);
-int calcola_valore(int *sol, options_t val, int nr, int nc);
+int calcola_best(int pos, options_t *val, int *sol, int *b_sol, int *mark, int n, int nr, int nc, int max);
+int calcola_valore(int *sol, options_t *val, int nr, int nc);
+void ruota(int *val_O, int *val_V, char *tipo_O, char *tipo_V);
 
 int main(){
     options_t *options;
@@ -41,6 +42,8 @@ int main(){
     carica_board(fp, board, options);
     fclose(fp);
 
+    mostra_best(board, options);
+
     free(options->tiles);
     free(board->tiles);
     free(options);
@@ -55,14 +58,14 @@ void carica_tiles(FILE *fp, options_t *options){
     options->tiles = (tile_t *) malloc(options->n*sizeof(tile_t));
     
     for(i=0; i<options->n; i++){
-        fscanf(fp, " %c %d %c %d", &options->tiles[i].tipo_V, &options->tiles[i].val_V, &options->tiles[i].tipo_O, &options->tiles[i].val_O);
+        fscanf(fp, " %c %d %c %d", &options->tiles[i].tipo_O, &options->tiles[i].val_O, &options->tiles[i].tipo_V, &options->tiles[i].val_V);
         options->tiles[i].rot=0;
         options->tiles[i].blocked=0;
     }
 }
 
 void carica_board(FILE *fp, board_t *board, options_t *options){
-    int i, j, t, r;  
+    int i, j, t, r;
 
     fscanf(fp, "%d %d", &board->R, &board->C);
 
@@ -73,67 +76,129 @@ void carica_board(FILE *fp, board_t *board, options_t *options){
             fscanf(fp, "%d/%d", &t, &r);
             if( t==-1 || r==-1 )
                 board->tiles[board->C*i+j] = -1;
-            board->tiles[board->C*i+j] = t;
-            options->tiles[t].blocked=1;
-            options->tiles[t].rot=r;
+            else{
+                board->tiles[board->C*i+j] = t;
+                options->tiles[t].blocked=1;
+                options->tiles[t].rot=r;
+                if(r==1){ //scambio i valori
+                    ruota(&(options->tiles[t].val_O), &(options->tiles[t].val_V), &(options->tiles[t].tipo_O), &(options->tiles[t].tipo_V));
+                }
+            }
         }
     }
 }
 
 void mostra_best(board_t *board, options_t *options){
-    int i, j, *sol, *b_sol, best_val, *mark;
+    int i, j, *sol, *b_sol, best_val=0, *mark;
 
     sol = (int *) malloc((board->R*board->C)*sizeof(int));
+    b_sol = (int *) malloc((board->R*board->C)*sizeof(int));
     for(i=0; i<board->R; i++)
-        for(j=0; j<board->C; j++)
+        for(j=0; j<board->C; j++){
             sol[board->C*i+j] = board->tiles[board->C*i+j];
+            b_sol[board->C*i+j] = board->tiles[board->C*i+j];
+        }
 
-    mark = (int *) calloc(options->n,sizeof(int));
+    mark = (int *) malloc(options->n*sizeof(int));
+    for(i=0; i<options->n; i++){
+        if(options->tiles[i].blocked==1)
+            mark[i]=-1;
+        else
+            mark[i]=0;
+    }
 
-    best_val = calcola_best(0, options, sol, board->tiles, mark, options->n, board->R, board->C);
+    best_val = calcola_best(0, options, sol, b_sol, mark, options->n, board->R, board->C, best_val);
 
-    printf("La soluzione migliore ha il punteggio massimo di %d ed è: \n", &best_val);
+    printf("La soluzione migliore ha il punteggio massimo di %d ed è: \n", best_val);
     for(i=0; i<board->R; i++){
         for(j=0; j<board->C; j++){
-            printf(" (Tubo verticale di tipo:%d e valore:%d)", &(options->tiles[b_sol[board->C*i+j]].tipo_V), &(options->tiles[b_sol[board->C*i+j]].tipo_O)); 
+            printf(" (Tubo verticale t:%c, v:%d--", options->tiles[b_sol[board->C*i+j]].tipo_V, options->tiles[b_sol[board->C*i+j]].val_V);
+            printf(" Tubo orizzontale t:%c, v:%d) ", options->tiles[b_sol[board->C*i+j]].tipo_O, options->tiles[b_sol[board->C*i+j]].val_O); 
         }
         printf("\n");
     }
 
+    free(b_sol);
     free(sol);
     free(mark);
 }
 
-int calcola_best(int pos, options_t *val, int *sol, int *b_sol, int *mark, int n, int nr, int nc){
-    int  i, max=0, c_val;
+int calcola_best(int pos, options_t *val, int *sol, int *b_sol, int *mark, int n, int nr, int nc, int max){
+    int  i, c_val, j;
     if(pos>=(nr*nc)){
-        //calcolo del valore della soluzione ottenuta
-        //se maggiore aggiorno max e b_sol
+        c_val = calcola_valore(sol, val, nr, nc);
+        if(c_val > max){
+            max = c_val;
+            for(i=0; i<nr; i++)
+                for(j=0; j<nc; j++)
+                    b_sol[nc*i+j] = sol[nc*i+j];
+        }
         return max;
     }
     for(i=0; i<n; i++){
-        if(mark[i]==0){
+        if(val->tiles[sol[pos]].blocked==1){
+            pos++;
+        }
+        else if (mark[i]==0){
             mark[i] = 1; 
             sol[pos] = i;
             val->tiles[i].rot=0;
-            max = calcola_best(pos+1, val, sol, b_sol, mark, n, nr, nc); //prima ricorsione (non ruotato)
-            mark[i]=2;
+            max = calcola_best(pos+1, val, sol, b_sol, mark, n, nr, nc, max); //prima ricorsione (non ruotato)
             val->tiles[i].rot=1;
-            max = calcola_best(pos+1, val, sol, b_sol, mark, n, nr, nc); //seconda ricorsione (ruotato)
+            ruota(&(val->tiles[i].val_O), &(val->tiles[i].val_V), &(val->tiles[i].tipo_O), &(val->tiles[i].tipo_V)); //ruoto i tubi
+            max = calcola_best(pos+1, val, sol, b_sol, mark, n, nr, nc, max); //seconda ricorsione (ruotato)
+            ruota(&(val->tiles[i].val_O), &(val->tiles[i].val_V), &(val->tiles[i].tipo_O), &(val->tiles[i].tipo_V));   //li riporto nella posizone originale
             mark[i] = 0;
         }
     }
     return max;
 }
 
-int calcola_valore(int *sol, options_t val, int nr, int nc){
-    int i, j, result=0;
+int calcola_valore(int *sol, options_t *val, int nr, int nc){
+    int i, j, tot=0, sum_o=0, sum_v=0, flag=1;
+    char tipo_o, tipo_v;
 
-    for(i=0; i<nr; i++){
-        for(j=0; j<nc; j++){
-            
+
+    for(i=0; i<nr; i++){    //calcolo le somme orizzontali
+            sum_o = val->tiles[nc*i].val_O;
+            tipo_o = val->tiles[nc*i].tipo_O;
+        for(j=1; j<nc && flag==1; j++){
+                if(tipo_o == val->tiles[nc*i+j].tipo_O)
+                    sum_o += val->tiles[nc*i+j].val_O;
+                else{
+                flag=0;
+                sum_o=0;
+                }
         }
+        tot += sum_o;               
     }
 
-    return result;
+    flag=1;
+
+    for(j=0; j<nc; j++){ //calcolo le somme verticali
+            sum_v = val->tiles[j].val_V;
+            tipo_v = val->tiles[j].tipo_V;
+        for(i=1; i<nr && flag==1; i++){
+                if(tipo_v == val->tiles[nc*i+j].tipo_V)
+                    sum_v += val->tiles[nc*i+j].val_V;
+                else{
+                    flag=0;
+                    sum_v=0;
+                }
+        }
+        tot += sum_v; 
+    }
+    return tot;
+}
+
+void ruota(int *val_O, int *val_V, char *tipo_O, char *tipo_V){
+    int val_tmp;
+    char tipo_tmp;
+
+    val_tmp = *val_O;
+    *val_O = *val_V;
+    *val_V = val_tmp;
+    tipo_tmp = *tipo_O;
+    *tipo_O = *tipo_V;
+    *tipo_V = tipo_tmp;
 }
